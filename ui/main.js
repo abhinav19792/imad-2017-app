7062662b348d7f12836e62d8bd1ce3b7cdff780a
abhinav19792 <abhinav81728@gmail.com>
2017-10-03 00:55:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,5 +20,5 @@ button.onclick = function() {
     
     // Make the request
     request.open('GET', 'http://abhinav81728.imad.hasura-app.io/counter', true);
-    request, send(null);
+    request.send(null);
 };

5823f21a3211ce0ddf63031de94759c659b628a0
abhinav19792 <abhinav81728@gmail.com>
2017-10-04 01:47:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 var config = {
     user: 'abhinav81728',
@@ -14,6 +15,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 function createTemplate (data) {
     
@@ -74,6 +76,25 @@ function hash (input, salt) {
 app.get('/hash/:input', function(req, res) {
    var hashedString = hash(req.params.input, 'this-is-some-random-string'); 
    res.send(hashedString);
+});
+
+app.post('/create-user', function(req, res) {
+   // Username and password
+   // {"username": "abhinav", "password": "password"}
+   // JSON 
+   var username = req.body.username;
+   var password = req.body.password;
+   
+   var salt = crypto.RandomBytes(128).toString('hex');
+   var dbString = hash(password, salt);
+   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function(err, result) {
+       if (err) {
+           res.status(500).send(err.toString());
+       }
+       else {
+           res.send('User successfully created: ' + username);
+       }       
+   });
 });
 
 var pool = new Pool(config);

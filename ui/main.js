e3d2cb5e4a20194bf4ec298728025d64c0d5ad11
abhinav19792 <abhinav81728@gmail.com>
2017-10-04 02:58:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,8 +24,7 @@ button.onclick = function() {
 };
 
 // Submit name
-
-var submit = document.getElementById('submit_btn');
+var submit = document.getElementById('submit_btn1');
 submit.onclick = function() {
     
         // Create a request object 
@@ -54,5 +53,38 @@ submit.onclick = function() {
     var name = nameInput.value;
     request.open('GET', 'http://abhinav81728.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);    
+    
+};
+
+// Submit Username/Password to login
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+    
+        // Create a request object 
+    var request = new XMLHttpRequest();
+    
+    // Capture the response and store it in variable.
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            // Take some action
+            if(request.status === 200) {
+                console.log('user logged in');
+                alert('Logged in Successfully');
+            } else if (request.status === 403) {
+                alert('Username/Password is incorrect! Please try again');
+            } else if (request.status === 500) {
+                alert('Something went wrong on the server');
+            }
+        }
+    };
+    
+    // Make the request
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST', 'http://abhinav81728.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));    
     
 };
